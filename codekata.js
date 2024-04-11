7578a7aaaa67f82df0cdab576f1626d0d9c99644
0gda <infinitetech3446@gmail.com>
2024-04-11 12:35:02 +0200

    This is a school project 11/4/20204

---
+++ codekata.js
@@ -0,0 +1,5 @@
+let adress = "Calle extremadura 6 puerta 6 piso 1,2";
+
+let test = Number(adress)
+console.log(test);
+return adress;
---
+++ funcion.js
@@ -0,0 +1,10 @@
+
+
+(function tester(testval) {
+    return("hello " + testval);
+})("test");
+
+
+let result = tester("test");
+
+console.log(result);
