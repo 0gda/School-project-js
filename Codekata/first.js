0c7b8ecda1c4573f431a18cd2c55314fdbafdcad
0gda <infinitetech3446@gmail.com>
2024-04-18 12:47:39 +0200

    18/4/2024 started operators

---
+++ Codekata/first.js
@@ -0,0 +1,5 @@
+let firstname = "Testname";
+let lastname = "testlastname";
+const fullname = '${testname} ${testlastname}';
+
+console.log(fullname);
---
+++ Codekata2/Operator.js
@@ -0,0 +1,13 @@
+let myFruit = "watermelon";
+
+switch (myFruit) {
+    case "watermelon":
+        console.log("Big fruit");
+        break;
+}
+
+let fruits = new Set(["lemon","watermelon"]);
+let test = (fruits.has(myFruit)) ? "YES" : "NO";
+
+console.log(test);
+
